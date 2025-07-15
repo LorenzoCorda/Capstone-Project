8b45570b2aba52d84ac35cb94119dedacec009cd
Lorenzo <cordalorenzowork@gmail.com>
2025-07-15 02:18:43 +0200

    fix notfound

--- Frontend/frontend/pages/NotFoundPage.jsx
+++ Frontend/frontend/pages/NotFoundPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Navigation from "../src/components/navigation/Navigation";
 import Footer from "../src/components/footer/Footer";
+import { OctagonAlert } from "lucide-react";
 
 const NotFoundPage = () => {
   return (
@@ -10,15 +11,16 @@ const NotFoundPage = () => {
         style={{ backgroundColor: "#FFFFFF" }}
         className="flex custom-bg flex-col items-center justify-center min-h-[70vh] text-center px-4 py-12"
       >
-        <h1 className="text-6xl font-bold text-blue-600"></h1>
-        <img
-          src="https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg"
-          alt=""
-        />
-        <h2 className="text-2xl font-semibold ">Pagina non trovata</h2>
-        <p className=" text-gray-600">
+        <OctagonAlert size={300} />
+
+        <h1 className="custom-question text-6xl font-bold mt-2 mb-3">404</h1>
+
+        <h2 className="custom-question text-2xl font-semibold mt-4 ">
+          Pagina non trovata
+        </h2>
+        <h3 className=" text-gray-600 mt-3">
           La pagina che stai cercando non esiste o è stata spostata.
-        </p>
+        </h3>
         <Link
           to="/"
           className="bg-blue-600 text-white px-6 py-3 rounded hover:bg-blue-700 transition"
